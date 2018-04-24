19d36b42565c7e2ce58b3646d41ee0d0e8b6d423
Michael Eby <michaeleby1@gmail.com>
2018-04-24 01:43:04 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,9 +1,10 @@
 function forloop(array) {
   for (let i = 0; i < 25; i++)
   if (i === 1) {
-  return "I am 1 strange loop.")
+  return ("I am 1 strange loop.")
   }
   else {
-  return (`I am ${i} strange loops.`)
+  console.log(`I am ${i} strange loops.`)
   }
 }
+return array.push(`I am ${i} strange loops.`)
